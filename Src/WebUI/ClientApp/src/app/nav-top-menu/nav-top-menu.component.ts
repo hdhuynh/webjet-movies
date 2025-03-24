8142c2910a591c5b81a6aa016d966455fcef9f6b
Hong Dat Huynh <hong.huynh@planetinnovation.com.au>
2025-03-25 00:48:34 +1100

    Use Webjet logo and color according to style guide

--- Src/WebUI/ClientApp/src/app/nav-top-menu/nav-top-menu.component.ts
+++ Src/WebUI/ClientApp/src/app/nav-top-menu/nav-top-menu.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav-top-menu',
-  templateUrl: './nav-top-menu.component.html'
+  templateUrl: './nav-top-menu.component.html',
+  styleUrls: ["./nav-top-menu.component.css"]
 })
 export class NavTopMenuComponent {
 }
